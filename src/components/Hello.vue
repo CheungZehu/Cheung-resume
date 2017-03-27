018afb886e5f4fb8789bbdb277f3bd435c81ee90
CheungZehu <845517336@qq.com>
2017-03-27 21:25:15 +0800

    ...

---
+++ src/components/Hello.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li v-for="item in resData">
+        <p>{{item.name}}</p>
+        <p>{{item.age}}</p>
+        <p>{{item.email}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      resData: []
+    }
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      this.resume();
+    })
+  },
+  methods: {
+    resume: function () {
+      this.$http.get('./../static/resume.json')
+        .then(response => {
+          alert(response.body.result);
+          this.resData = response.body.result;
+        })
+        .catch(function (response) {
+          console.log(response)
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
